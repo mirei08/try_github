98592d9586517c303ba46df7a41f842cc692236d
mirei08 <mirei@occ.co.jp>
2019-05-08 11:05:21 +0900

    Main クラスの編集

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,6 +10,11 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello GitHub!");
+            Console.WriteLine("画面を閉じるには何かキーを入力してください。");
+            Console.ReadKey();
+
         }
     }
 }
